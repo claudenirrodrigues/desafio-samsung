144753b10720b6a887e97b27908f0d6aa50a453a
claudenir <claudenir.rodrigues.oliveira@gmail.com>
2022-08-06 22:01:14 -0300

    Iniciando aplicação

---
+++ samsung-evaluation-api/src/test/java/com/samsung/evaluation/api/SamsungEvaluationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsung.evaluation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsungEvaluationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
